643b96840bfdbe8fa57b91fa2ed991ca8f2875a4
EmeraldWither <68785503+EmeraldWither@users.noreply.github.com>
2025-06-23 21:13:57 -0400

    add scripts and server daemon

---
+++ ServerDaemon/src/test/java/org/emeraldcraft/serverdaemon/ServerDaemonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.emeraldcraft.serverdaemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerDaemonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
