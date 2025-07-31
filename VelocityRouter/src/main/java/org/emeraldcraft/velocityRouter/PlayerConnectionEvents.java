7f025822b24bdb6d34f32e51c6a828e33ca45bc8
EmeraldWither <68785503+EmeraldWither@users.noreply.github.com>
2025-07-31 10:10:58 -0400

    readd whitelist and maintenance mode

--- VelocityRouter/src/main/java/org/emeraldcraft/velocityRouter/PlayerConnectionEvents.java
+++ VelocityRouter/src/main/java/org/emeraldcraft/velocityRouter/PlayerConnectionEvents.java
@@ -1,7 +1,10 @@
 package org.emeraldcraft.velocityRouter;
 
+import com.velocitypowered.api.event.ResultedEvent;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.connection.DisconnectEvent;
+import com.velocitypowered.api.event.connection.LoginEvent;
+import com.velocitypowered.api.event.connection.PreLoginEvent;
 import com.velocitypowered.api.event.player.ServerConnectedEvent;
 import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.server.RegisteredServer;
@@ -36,7 +39,47 @@ public class PlayerConnectionEvents {
             }
         }).delay(Duration.ofSeconds(2)).schedule();
     }
+    @Subscribe
+    public void onPreLogin(PreLoginEvent event) {
+        if (VelocityRouter.getConfiguration().isMaintenance()) {
+            PreLoginEvent.PreLoginComponentResult denied = PreLoginEvent.PreLoginComponentResult.denied(Component.text(VelocityRouter.getConfiguration().getMaintenanceMessage()).color(NamedTextColor.RED));
+            event.setResult(denied);
+            VelocityRouter.getLogger().info("[prelogin] Player %s attempted to join during maintenance mode.".formatted(event.getUsername()));
+            return;
+        }
+        PreLoginEvent.PreLoginComponentResult denied = PreLoginEvent.PreLoginComponentResult.denied(Component.text("You are not whitelisted to be part of the network.").color(NamedTextColor.RED));
+        if(event.getUniqueId() == null) {
+            event.setResult(denied);
+            VelocityRouter.getLogger().warn("[prelogin] Unable to find a UUID for the ip '%s'; has been kicked.".formatted(event.getConnection().getRemoteAddress().getHostString()));
+            return;
+        }
+        if (!VelocityRouter.getConfiguration().getAllowList().contains(event.getUniqueId().toString())) {
+            event.setResult(denied);
+            VelocityRouter.getLogger().warn("[prelogin] Player %s (UUID %s) attempted to join but is not on the allowlist.".formatted(event.getUsername(), event.getUniqueId()));
+        }
+    }
+    @Subscribe
+    public void onPlayerLogin(LoginEvent event) {
+        if (VelocityRouter.getConfiguration().isMaintenance()) {
+            ResultedEvent.ComponentResult denied = LoginEvent.ComponentResult.denied(Component.text(VelocityRouter.getConfiguration().getMaintenanceMessage()));
+            event.getPlayer().disconnect(denied.getReasonComponent().get());
+            event.setResult(denied);
+            VelocityRouter.getLogger().info("Player %s attempted to join during maintenance mode.".formatted(event.getPlayer().getUsername()));
+            return;
+        }
+        //check for allowlist
 
+        if (!VelocityRouter.getConfiguration().getAllowList().contains(event.getPlayer().getUniqueId().toString())) {
+            ResultedEvent.ComponentResult denied = LoginEvent.ComponentResult.denied(Component.text("You are not whitelisted to be part of the network.").color(NamedTextColor.RED));
+            event.getPlayer().disconnect(denied.getReasonComponent().get());
+            event.setResult(denied);
+            VelocityRouter.getLogger().warn("[login] Player %s (UUID %s) attempted to join but is not on the allowlist.".formatted(event.getPlayer().getUsername(), event.getPlayer().getUniqueId().toString()));
+            return;
+        }
+
+        //check for selected server
+        event.setResult(ResultedEvent.ComponentResult.allowed());
+    }
     @Subscribe
     public void onPlayerSwitch(ServerConnectedEvent event) {
         lastServer.put(event.getPlayer(), event.getServer());
